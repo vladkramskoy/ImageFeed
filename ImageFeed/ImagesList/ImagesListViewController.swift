796b0c7e347ad037d62159dcc143385d44a7ba56
Vlad Kramskoy <vkto5609@gmail.com>
2024-01-23 22:18:52 +0300

    Добавлен новый функционал: [Лента с фото]

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        
+        return formatter
+    }()
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        
+        if let image = UIImage(named: photosName[indexPath.row]) {
+            cell.cellImage.image = image
+        }
+        
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLaked = indexPath.row % 2 == 0
+        let likeImage = isLaked ? UIImage(named: "likeButtonOn") : UIImage(named: "likeButtonOff")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
