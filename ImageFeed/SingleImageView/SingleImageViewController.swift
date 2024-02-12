517230abf734ffe03eecec2f70ebe72dd5c085f5
Vlad Kramskoy <vkto5609@gmail.com>
2024-02-12 20:45:55 +0300

    Реализовано взаимодействие с изображением с помощью zoom

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -5,18 +5,56 @@ final class SingleImageViewController: UIViewController {
         didSet { // Предусматривем ситуацию, если нужно будет подменить изображение в уже показанном контроллере извне
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
-    @IBOutlet var imageView: UIImageView!
-    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        imageView.frame.size = image.size
+        
+        scrollView.minimumZoomScale = 0.1 // 10%
+        scrollView.maximumZoomScale = 1.25 // 125%
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: true)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: true)
     }
     
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
+    @IBAction private func didTapSharingButton(_ sender: UIButton) {
+        let share = UIActivityViewController(activityItems: [image ?? UIImage()], applicationActivities: nil)
+        present(share, animated: true, completion: nil)
+    }
     
 }
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+}
+
