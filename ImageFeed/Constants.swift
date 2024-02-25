1f0a2dc993b4d1f75aea59473a1b04edd48c9ad1
Vlad Kramskoy <vkto5609@gmail.com>
2024-02-25 11:34:00 +0300

    Создан файл Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "d13Iiq9tAYRIQATpxbtWln8UVIHEu33vHaydrvs0bMQ"
+let SecretKey = "xw6uqFxKlEJmFbi7DQHpCUByKPnC_G7lqi43jYnM5uo"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
