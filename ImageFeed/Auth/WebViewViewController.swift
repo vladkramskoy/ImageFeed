86ed660c448e52957144ac2a4a9fa9727839a00d
Vlad Kramskoy <vkto5609@gmail.com>
2024-02-26 14:23:06 +0300

    Создан url с помощью URLComponents, к webView добавлен запрос

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -4,4 +4,33 @@ import WebKit
 class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
     
+    enum WebViewConstants {
+        static let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
+    
+    override func viewDidLoad() {
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.UnsplashAuthorizeURLString) else {
+            print("Ошибка при создании URLComponents")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            print("Ошибка: urlComponents равен nil")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
