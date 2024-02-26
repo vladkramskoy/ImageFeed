d21bac03a0cacacce76dd3b494d515a5bbb01fba
Vlad Kramskoy <vkto5609@gmail.com>
2024-02-26 10:45:20 +0300

    Добавлен webView, кастомизирован backBarButtonItem

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,5 +1,16 @@
 import UIKit
 
 class AuthViewController: UIViewController {
+    private let ShowWebViewIdentifier = "ShowWebView"
     
+    override func viewDidLoad() {
+        configureBackButton()
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black (iOS)")
+    }
 }
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+import WebKit
+
+class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+    
+}
