fa3524761686014fcef2458fc0515cc451f4e68b
Vlad Kramskoy <vkto5609@gmail.com>
2024-05-29 23:24:57 +0300

    Создан AuthHelper

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -20,7 +20,8 @@ final class AuthViewController: UIViewController {
             guard
                 let webViewViewController = segue.destination as? WebViewViewController
             else { fatalError("Failed to prepare for \(showWebViewIdentifier)") }
-            let webViewPresenter = WebViewPresenter()
+            let authHelper = AuthHelper()
+            let webViewPresenter = WebViewPresenter(authHelper: authHelper)
             webViewViewController.presenter = webViewPresenter
             webViewPresenter.view = webViewViewController
             webViewViewController.delegate = self
--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -9,10 +9,11 @@ public protocol WebViewPresenterProtocol {
 
 final class WebViewPresenter: WebViewPresenterProtocol {
     weak var view: WebViewViewControllerProtocol?
+    var authHelper: AuthHelperProtocol
     
-//    enum WebViewConstants {
-//        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-//    }
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
     
     func viewDidLoad() {
         loadAuthView()
@@ -27,43 +28,17 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code(from url: URL) -> String? {
-        if let urlComponents = URLComponents(string: url.absoluteString),
-           urlComponents.path == "/oauth/authorize/native",
-           let items = urlComponents.queryItems,
-           let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
     }
-    
+
     private func shouldHideProgress(for value: Float) -> Bool {
         abs(value - 1.0) <= 0.0001
     }
     
     private func loadAuthView() {
-        guard var urlComponents = URLComponents(string: AuthConfiguration.standart.authURLString) else {
-            print("Error creating URLComponents")
-            return
-        }
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        
-        guard let url = urlComponents.url else {
-            print("URLComponents is equal to nil")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        didUpdateProgressValue(0)
+        guard let request = authHelper.authRequest() else { return }
         
         view?.load(request: request)
+        didUpdateProgressValue(0)
     }
 }
